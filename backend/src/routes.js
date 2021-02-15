b0dac8e2cf8d882ea936a327306df0f3658f84a8
Deivis <deivisutp@gmail.com>
2021-02-15 12:52:04 -0300

    frontend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,36 +1,34 @@
-const routes = require('express').Router();
-const multer = require('multer');
-const multerConfig = require('./config');
+const routes = require("express").Router();
+const multer = require("multer");
+const multerConfig = require("./config/multer");
 
-const Post = require('./models/post');
+const Post = require("./models/Post");
 
-routes.get("/posts", multer().single('file'), async (req,res) => {
-    const posts = await Post.find();
+routes.get("/posts", async (req, res) => {
+  const posts = await Post.find();
 
-    return res.json(posts);
+  return res.json(posts);
 });
 
-routes.post("/posts", multer().single('file'), async (req,res) => {
-    const { originalname: name, size, key, location: url = "" } = req.file;
-    const post = await Post.create({
-        name,
-        size,
-        key,
-        url,
-    });
-    return res.json(post);
+routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+  const { originalname: name, size, key, location: url = "" } = req.file;
+
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url
+  });
+
+  return res.json(post);
 });
 
-routes.get("/", multer().single('file'), (req,res) => {
-    return res.json({ hello: "Rocket" });
-});
+routes.delete("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
 
-routes.delete("/posts/:id", multer().single('file'), async (req,res) => {
-    const post = await Post.findById(req.params.id);
+  await post.remove();
 
-    await post.remove();
-
-    return res.json({ message: "Sucess"});
+  return res.send();
 });
 
 module.exports = routes;
